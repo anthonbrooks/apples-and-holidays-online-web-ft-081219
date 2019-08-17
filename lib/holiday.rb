5cd54a14d41ec41ba5c8e8414a8361ad12fc050e
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-17 18:20:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,6 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].map do |holiday, supplies|
     supplies.flatten
   end
-  supplies.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
