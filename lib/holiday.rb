6ddbc82437c9f53f4a4eb2725605eed32499d9b4
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-17 18:34:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,6 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
+      return holiday.flatten
     end
   end
 end
